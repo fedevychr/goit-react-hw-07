e64fb284c6014058facdf694689edda081eec8dd
Roman Fedevych <fedevychr@gmail.com>
2024-06-04 14:58:36 +0300

    comment persist in store and main

--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { Provider } from "react-redux";
-import { PersistGate } from "redux-persist/integration/react";
-import { persistor, store } from "./redux/store.js";
+// import { PersistGate } from "redux-persist/integration/react";
+import {
+  // persistor,
+  store,
+} from "./redux/store.js";
 import "modern-normalize";
 
 import App from "./components/App.jsx";
@@ -12,9 +15,9 @@ import "./index.css";
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <Provider store={store}>
-      <PersistGate persistor={persistor}>
-        <App />
-      </PersistGate>
+      {/* <PersistGate persistor={persistor}> */}
+      <App />
+      {/* </PersistGate> */}
     </Provider>
   </React.StrictMode>
 );
